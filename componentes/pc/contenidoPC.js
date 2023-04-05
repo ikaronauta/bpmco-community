055a3b33e1e60ee3a21ffd4720a9b1cf883148de
ikaronauta <81399721+ikaronauta@users.noreply.github.com>
2023-04-05 18:38:47 -0500

    Se agrega contenido nuevo

--- componentes/pc/contenidoPC.js
+++ componentes/pc/contenidoPC.js
@@ -1,6 +1,50 @@
+import { posts } from "../../assets/data/data.js";
+
 export function contenidoPC() {
+  let i = 0;
   let contenidoPC = document.createElement("div");
   contenidoPC.className = "contenido-pc";
 
+  posts.reverse().map((post) => {
+    i++;
+    contenidoPC.appendChild(vistaPost(post, i));
+  });
+
   return contenidoPC;
+}
+
+function vistaPost(post, i) {
+  let div = document.createElement("div");
+  div.className = "contenedor-post";
+
+  let contenedorSuperior = document.createElement("div");
+  contenedorSuperior.className = "contenedor-superior";
+
+  let tituloPost = document.createElement("h3");
+  tituloPost.innerHTML = post.titulo;
+
+  let datosPost = document.createElement("p");
+  datosPost.innerHTML = `${post.usuario} - ${post.fecha}`;
+
+  contenedorSuperior.appendChild(tituloPost);
+  contenedorSuperior.appendChild(datosPost);
+
+  let contenedorInferior = document.createElement("div");
+  contenedorInferior.className = "contenedor-inferior";
+
+  let img = document.createElement("img");
+  img.className = "imagen-portada";
+  img.src = post.portada;
+
+  let p = document.createElement("p");
+  p.className = "texto-post";
+  p.innerHTML = post.post;
+
+  contenedorInferior.appendChild(img);
+  contenedorInferior.appendChild(p);
+
+  div.appendChild(contenedorSuperior);
+  div.appendChild(contenedorInferior);
+
+  return div;
 }
--- componentes/pc/navPC.js
+++ componentes/pc/navPC.js
@@ -1,18 +1,60 @@
 export function navPC() {
   let navPC = document.createElement("nav");
-  navPC.className = "nav-pc";
+  navPC.className = "nav-pc menu-cerrado";
 
   let icono = document.createElement("img");
   icono.className = "open";
 
-  icono.addEventListener("click", () => openClose(icono));
+  icono.addEventListener("click", () => openClose(icono, navPC));
+
+  let contenedorInternoNavPC = document.createElement("nav");
+  contenedorInternoNavPC.className = "contenedor-interno-navPC";
+
+  contenedorInternoNavPC.appendChild(cajonRecuerdos());
+  contenedorInternoNavPC.appendChild(cajonMemes());
 
   navPC.appendChild(icono);
+  navPC.appendChild(contenedorInternoNavPC);
+
+  setTimeout(() => pulsacionesNavPC(navPC), 5000);
+  setInterval(() => pulsacionesNavPC(navPC), 60000);
 
   return navPC;
 }
 
-function openClose(icono) {
+function openClose(icono, navPC) {
   icono.classList.toggle("open");
   icono.classList.toggle("close");
+
+  navPC.classList.toggle("menu-abierto");
+  navPC.classList.toggle("menu-cerrado");
+}
+
+function pulsacionesNavPC(item) {
+  item.classList.add("pulsacionNavPC");
+  setTimeout(() => {
+    item.classList.remove("pulsacionNavPC");
+
+    //Espera
+    setTimeout(() => {
+      item.classList.add("pulsacionNavPC");
+
+      //Nueva espera
+      setTimeout(() => item.classList.remove("pulsacionNavPC"), 100);
+    }, 250);
+  }, 500);
+}
+
+function cajonRecuerdos() {
+  let div = document.createElement("div");
+  div.className = "cajon-recuerdos";
+
+  return div;
+}
+
+function cajonMemes() {
+  let div = document.createElement("div");
+  div.className = "cajon-memes";
+
+  return div;
 }
