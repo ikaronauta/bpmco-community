cb96be30cf0dbee1de83f648568c2b445c3fb4d7
ikaronauta <81399721+ikaronauta@users.noreply.github.com>
2023-04-12 12:26:00 -0500

    Se ajustan rutas de las imagenes

--- assets/data/data.js
+++ assets/data/data.js
@@ -5,7 +5,7 @@ export const posts = [
     titulo: "Presentación",
     post: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras id lacus sodales, volutpat risus eu, vestibulum nulla. Phasellus consectetur suscipit maximus. Ut pulvinar sodales nisi nec malesuada. Aliquam non vulputate ipsum, id sollicitudin justo. Integer luctus mattis leo, eu pulvinar ligula fringilla vitae. Nulla nec velit id purus varius faucibus. Pellentesque porttitor, tortor nec dictum efficitur, diam elit semper nisl, ut laoreet purus purus vel libero. Vivamus venenatis enim id elementum volutpat. <br><br> Vivamus sed enim placerat, tincidunt elit congue, dignissim mauris. Praesent volutpat lorem vitae nulla ullamcorper scelerisque. Aenean et eros lacus. Donec sed quam vitae lectus pellentesque ullamcorper eget eget odio. Quisque in ligula non ante sagittis sollicitudin eget et mi.Proin eu lacinia erat. Integer dapibus, felis ut pharetra pretium, nunc dolor vestibulum turpis, vel luctus tellus felis eget neque. Phasellus mattis leo ut sem euismod consequat. Curabitur mauris erat, pretium ut blandit molestie, aliquam quis lacus. Sed pharetra pharetra scelerisque. Suspendisse eget tortor sit amet velit molestie vulputate eu placerat justo. Ut in sapien arcu.Sed ornare ac massa et dapibus. Aliquam tristique nisl id eleifend imperdiet. Fusce scelerisque et urna quis porta. <br><br> Vivamus sed enim placerat, tincidunt elit congue, dignissim mauris. Praesent volutpat lorem vitae nulla ullamcorper scelerisque. Aenean et eros lacus. Donec sed quam vitae lectus pellentesque ullamcorper eget eget odio. Quisque in ligula non ante sagittis sollicitudin eget et mi.Proin eu lacinia erat. Integer dapibus, felis ut pharetra pretium, nunc dolor vestibulum turpis, vel luctus tellus felis eget neque. Phasellus mattis leo ut sem euismod consequat. Curabitur mauris erat, pretium ut blandit molestie, aliquam quis lacus. Sed pharetra pharetra scelerisque. Suspendisse eget tortor sit amet velit molestie vulputate eu placerat justo. Ut in sapien arcu.Sed ornare ac massa et dapibus. Aliquam tristique nisl id eleifend imperdiet. Fusce scelerisque et urna quis porta.",
     usuario: "Gerencia BPMco",
-    portada: "/assets/imagenes/posts/imagen1.jpg",
+    portada: "./assets/imagenes/posts/imagen1.jpg",
     imagenes: ["/assets/imagenes/posts/imagen1.jpg"],
   },
   {
@@ -14,7 +14,7 @@ export const posts = [
     titulo: "Felicitacion Comite Bienestar",
     post: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras id lacus sodales, volutpat risus eu, vestibulum nulla. Phasellus consectetur suscipit maximus. Ut pulvinar sodales nisi nec malesuada. Aliquam non vulputate ipsum, id sollicitudin justo. Integer luctus mattis leo, eu pulvinar ligula fringilla vitae. Nulla nec velit id purus varius faucibus. Pellentesque porttitor, tortor nec dictum efficitur, diam elit semper nisl, ut laoreet purus purus vel libero. Vivamus venenatis enim id elementum volutpat. <br><br> Vivamus sed enim placerat, tincidunt elit congue, dignissim mauris. Praesent volutpat lorem vitae nulla ullamcorper scelerisque. Aenean et eros lacus. Donec sed quam vitae lectus pellentesque ullamcorper eget eget odio. Quisque in ligula non ante sagittis sollicitudin eget et mi.Proin eu lacinia erat. Integer dapibus, felis ut pharetra pretium, nunc dolor vestibulum turpis, vel luctus tellus felis eget neque. Phasellus mattis leo ut sem euismod consequat. Curabitur mauris erat, pretium ut blandit molestie, aliquam quis lacus. Sed pharetra pharetra scelerisque. Suspendisse eget tortor sit amet velit molestie vulputate eu placerat justo. Ut in sapien arcu.Sed ornare ac massa et dapibus. Aliquam tristique nisl id eleifend imperdiet. Fusce scelerisque et urna quis porta. <br><br> Vivamus sed enim placerat, tincidunt elit congue, dignissim mauris. Praesent volutpat lorem vitae nulla ullamcorper scelerisque. Aenean et eros lacus. Donec sed quam vitae lectus pellentesque ullamcorper eget eget odio. Quisque in ligula non ante sagittis sollicitudin eget et mi.Proin eu lacinia erat. Integer dapibus, felis ut pharetra pretium, nunc dolor vestibulum turpis, vel luctus tellus felis eget neque. Phasellus mattis leo ut sem euismod consequat. Curabitur mauris erat, pretium ut blandit molestie, aliquam quis lacus. Sed pharetra pharetra scelerisque. Suspendisse eget tortor sit amet velit molestie vulputate eu placerat justo. Ut in sapien arcu.Sed ornare ac massa et dapibus. Aliquam tristique nisl id eleifend imperdiet. Fusce scelerisque et urna quis porta.",
     usuario: "Gerencia BPMco",
-    portada: "/assets/imagenes/posts/imagen2.jpg",
+    portada: "./assets/imagenes/posts/imagen2.jpg",
     imagenes: ["/assets/imagenes/posts/imagen1.jpg"],
   },
 ];
@@ -25,13 +25,13 @@ export const contenidoMenuFijo = [
     titulo: "Contenido Fijo 1",
     contenido:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde assumenda dolor tenetur quis libero aperiam facilis molestiae error impedit amet fugiat maiores odio, laboriosam voluptate explicabo saepe soluta ab esse!",
-    imagen: "/assets/imagenes/menu-fijo/11.png",
+    imagen: "./assets/imagenes/menu-fijo/11.png",
   },
   {
     id: 2,
     titulo: "Contenido Fijo 2",
     contenido:
       "Reiciendis deserunt saepe, quae sint deleniti, aut ad magnam eveniet aspernatur quia inventore, non consequuntur natus. Aperiam voluptatibus eligendi mollitia voluptas veniam impedit, incidunt excepturi ea fugit dolores natus rerum?",
-    imagen: "/assets/imagenes/menu-fijo/22.jpg",
+    imagen: "./assets/imagenes/menu-fijo/22.jpg",
   },
 ];
